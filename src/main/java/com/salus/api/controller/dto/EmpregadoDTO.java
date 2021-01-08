56f57a5669d0f0b08774ce58d03799b2df4c4a7d
SOLUTIS\felipe.ribeiro <felipe.ribeiro@solutis.com.br>
2021-01-08 09:14:31 -0300

    Implementando EmpregadoDTO

--- src/main/java/com/salus/api/controller/dto/EmpregadoDTO.java
+++ src/main/java/com/salus/api/controller/dto/EmpregadoDTO.java
@@ -1,5 +1,72 @@
 package com.salus.api.controller.dto;
 
-public class EmpregadoDTO {
+import com.salus.api.model.Empregado;
 
+public class EmpregadoDTO {
+	
+	private String nome;
+	private String cpf;
+	private String email;
+	private String cargo;
+	private Double salario;
+	
+	
+	public EmpregadoDTO(Empregado empregado) {
+		this.nome = empregado.getNome();
+		this.cpf = empregado.getCpf();
+		this.email = empregado.getEmail();
+		this.cargo = empregado.getCargo();
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getCargo() {
+		return cargo;
+	}
+
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+
+	public Double getSalario() {
+		return salario;
+	}
+
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
+	
 }
